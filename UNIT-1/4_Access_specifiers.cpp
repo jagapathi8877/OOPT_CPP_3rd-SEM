efe41ee305fb4ae2ba3e0d3c5a0e94e2a22c1388
jagapathi8877 <jagapathinamala@gmail.com>
2025-10-22 18:51:51 +0530

    Update 4_Access_specifiers.cpp

--- UNIT-1/4_Access_specifiers.cpp
+++ UNIT-1/4_Access_specifiers.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 using namespace std;
-class ClassA
-{
-	private:
-				int sno;  // it is private, not accessible outside
-	public:
-				int pincode;   // avaiable anywhere
-	protected:
-				int mobno;  // available only in Child class...
-	
+
+class A {
+private:
+    int privateVar = 10;
+
+protected:
+    int protectedVar = 20;
+
+public:
+    int publicVar = 30;
+
+    void display() {
+        cout << "Private = " << privateVar << endl;
+        cout << "Protected = " << protectedVar << endl;
+        cout << "Public = " << publicVar << endl;
+    }
 };
-class ClassB:public ClassA
-{
-	public:
-	void disp()
-	{
-		cout<<"Pincode"<<pincode;
-	}
-};
-int main()
-{
-	ClassA ob;
-	// ob.sno=100;  ERROR WILL COME - bcoz it is private
-	ob.pincode=123;
-	cout<<ob.pincode;  // accessibel bcoz it is public
-	ClassB obb;
-	obb.disp();
-	return 0;
+
+int main() {
+    A obj;
+    obj.display();
+    // Not allowed:
+    // cout << obj.privateVar;
+    // cout << obj.protectedVar;
+    cout << "Accessing public variable directly: " << obj.publicVar << endl;
+    return 0;
 }
