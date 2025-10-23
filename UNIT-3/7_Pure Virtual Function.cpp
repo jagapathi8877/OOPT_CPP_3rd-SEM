5379c637a59d51399b04ea6409fa52fb6d287c69
jagapathi8877 <jagapathinamala@gmail.com>
2025-10-23 09:51:13 +0530

    Update 7_Pure Virtual Function.cpp

--- UNIT-3/7_Pure Virtual Function.cpp
+++ UNIT-3/7_Pure Virtual Function.cpp
@@ -1,42 +1,69 @@
-//  Pure Virtual Function
+// C++ program to demonstrate Abstract Classes
+// and Pure Virtual Functions for calculating area
+
 #include <iostream>
-#include <cmath>
 using namespace std;
+
 // Abstract Base Class
 class Shape {
-public:    // Pure virtual function
-    virtual void area() = 0;	};
-// Derived Class: Circle
-class Circle : public Shape {
-    float radius;
-public:    Circle(float r) : radius(r) {}
-    		void area() {
-        		cout << "Area of Circle = " << 3.14159 * radius * radius << endl;
-   	 }	};
-// Derived Class: Rectangle
+public:
+    // Pure virtual function
+    virtual void area() = 0;  
+};
+
+// Derived class for Rectangle
 class Rectangle : public Shape {
     float length, breadth;
-public:	    Rectangle(float l, float b) : length(l), breadth(b) {}
-    		void area() {
-      		  cout << "Area of Rectangle = " << length * breadth << endl;
-    }	};
-// Derived Class: Triangle
+public:
+    Rectangle(float l, float b) {
+        length = l;
+        breadth = b;
+    }
+    void area() override {
+        cout << "Area of Rectangle: " << length * breadth << endl;
+    }
+};
+
+// Derived class for Circle
+class Circle : public Shape {
+    float radius;
+public:
+    Circle(float r) {
+        radius = r;
+    }
+    void area() override {
+        cout << "Area of Circle: " << 3.14 * radius * radius << endl;
+    }
+};
+
+// Derived class for Triangle
 class Triangle : public Shape {
     float base, height;
-public:	Triangle(float b, float h) : base(b), height(h) {}
-    	void area() {
-        	cout << "Area of Triangle = " << 0.5 * base * height << endl;
-   	 }	};
+public:
+    Triangle(float b, float h) {
+        base = b;
+        height = h;
+    }
+    void area() override {
+        cout << "Area of Triangle: " << 0.5 * base * height << endl;
+    }
+};
+
 int main() {
-    Shape *s; // Base class pointer
-    Circle c(5);
-    Rectangle r(4, 6);
-    Triangle t(3, 7);
-    s = &c;
-    s->area();
-    s = &r;
-    s->area();
-    s = &t;
-    s->area();
+    Shape* shapePtr; // Base class pointer
+
+    Rectangle rect(5, 4);
+    Circle cir(3);
+    Triangle tri(6, 2);
+
+    shapePtr = &rect;
+    shapePtr->area();
+
+    shapePtr = &cir;
+    shapePtr->area();
+
+    shapePtr = &tri;
+    shapePtr->area();
+
     return 0;
 }
